99481e33131b4c82d3a78ac05e62ac483b4468d4
nick800103 <keepprogress@gmail.com>
2020-12-15 11:02:41 +0800

    add point cut for getter/setter Combine and Apply

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,12 +12,25 @@ public class MyDemoLoggingAspect {
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 		
-	@Before("forDaoPackage()")
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	public void getter() {}
+	
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	public void setter() {}
+
+	// create point: include package ... exclude getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+	
+	
+	@Before("forDaoPackageNoGetterSetter()")
 	public void AddAccountAdvice() {		
 		System.out.println("\n=====>>> Excuting @Before advice on addAccount()");
 	}
 	
-	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytic() {
 		System.out.println("\n=====>>> Peforming API analytics");
 	}
